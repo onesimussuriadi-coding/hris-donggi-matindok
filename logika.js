c3a6344a5c47a16fdbf987d401e4fa085ebd977f
onesimussuriadi-coding <onesimus.suriadi@gmail.com>
2026-08-13 17:44:41 +0800

    Update logika.js

--- logika.js
+++ logika.js
@@ -223,11 +223,7 @@ function bukaModalHistori(noKaryawan) {
         elRekapMakan.innerText = (karyawan.totalMakanLemburRekap || 0) + " Kali";
     }
 
-    let elRekapMakan = document.getElementById('rekapDistMakanLembur');
-if (elRekapMakan) {
-    let totalMakanModal = (karyawan.makanPagi || 0) + (karyawan.makanSiang || 0) + (karyawan.makanMalam || 0);
-    elRekapMakan.innerText = totalMakanModal + " Kali";
-}
+    
     document.getElementById('modalHistori').classList.remove('hidden');
 }
 
